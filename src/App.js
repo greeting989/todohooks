a9c53373e7a61f1b65952e98f385762a9e17bfdc
greeting989 <65461110+greeting989@users.noreply.github.com>
2021-08-30 17:27:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Restaurants from './components/Basics/Restaurants';
-import UseState from './components/Hooks/useState';
-import UseEffect from './components/Hooks/useEffect';
-import UseReducer from './components/Hooks/useReducer';
-import Todo from './components/todoreact/todo';
+
 import Todopractice from './components/todoreact/todopractice';
 
 function App() {
   return (
     <Todopractice/>
-    // <Todo/>
-    // <UseReducer/>
-    // <UseEffect/>
-      // <UseState/>
-    //  <Restaurants/>
+   
   );
 }
 
